77672e4a03e637e487af8ff7d171b4b8e721f8db
Nadezhda Shmatova <nadezhda.shmatova@alg.team>
2022-08-15 23:45:55 +0300

    init

--- cypress/e2e/myTest/ShouldTestCase.cy.js
+++ cypress/e2e/myTest/ShouldTestCase.cy.js
@@ -2,6 +2,11 @@ describe('Checking Click Menu', () => {
     it('Проверка текста на корректность', () => {
         cy.visit('https://www.jetbrains.com/ru-ru/')
         cy.get('.main-menu-item', {timeout: 10000}).first().should('contain', 'Разработчикам')
+        cy.get('.main-menu-item').then(itemd => itemd[1]).should('contain', 'Командам')
+        cy.get('.main-menu-item').then(itemd => itemd[2]).should('contain', 'Для обучения')
+        cy.get('.main-menu-item').then(itemd => itemd[3]).should('contain', 'Решения')
+        cy.get('.main-menu-item').then(itemd => itemd[4]).should('contain', 'Поддержка')
+        cy.get('.main-menu-item').then(itemd => itemd[5]).should('contain', 'Магазин')
     })
 })
 
