cce6affca19985d260439c147311f61ce901cb44
Nadya <41388388+Testika@users.noreply.github.com>
2022-08-15 19:45:13 +0300

    Update ShouldTestCase.cy.js

--- cypress/e2e/myTest/ShouldTestCase.cy.js
+++ cypress/e2e/myTest/ShouldTestCase.cy.js
@@ -1,5 +1,5 @@
 describe('Checking Click Menu', () => {
-    it('Проверка текста на корректность', () => {
+    it('Проверка текста меню на корректность', () => {
         cy.visit('https://www.jetbrains.com/ru-ru/')
         cy.get('.main-menu-item', {timeout: 10000}).first().should('contain', 'Разработчикам')
     })
