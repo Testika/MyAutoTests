a795ababcce7dfd1f50cf286367174acbe1a2330
Nadya <41388388+Testika@users.noreply.github.com>
2022-08-31 21:35:15 +0300

    sela store_use fixture

--- cypress/e2e/myTest/sela_fixture.cy.js
+++ cypress/e2e/myTest/sela_fixture.cy.js
@@ -1,4 +1,4 @@
-describe('UseFixtureSela', () => {
+describe('UseFixture', () => {
     before('UseFixture',function (){
         cy.fixture('example').then(function(data){
             this.data=data
